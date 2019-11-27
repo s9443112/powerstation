f815c4425ca2f2461c84c00574bf8a887a57ff39
LEE Timothy <leetimothy@LEEde-MacBook-Pro.local>
2019-11-27 11:12:26 +0800

    fix mqtt

--- src/mqtt/mqtt.js
+++ src/mqtt/mqtt.js
@@ -3,7 +3,7 @@ var option = {
     username: "luhao",
     password: "a23387696"
 }
-const client = mqtt.connect("mqtt://luhao.com.tw",option)
+const client = mqtt.connect("mqtt://luhao.com.tw", option)
 
 client.on("connect", function () {
     console.log("Success connect mqtt broker.")
@@ -11,24 +11,30 @@ client.on("connect", function () {
         if (!err) {
             console.log("Success subscribe powerstation topic.")
         }
-        
-        setInterval(() => {
-            var hot = Math.floor(Math.random()*(100-70+1))+70;
-        var cold  = Math.floor(Math.random()*(20-0+1))+0;
-        var cold2  = Math.floor(Math.random()*(20-0+1))+0;
-        var vol = 5
-        var current = 1
-            var data = {
-                "station_id":1,
-                "hot_water_temp":hot,
-                "cold_water_temp":cold,
-                "cold_water_temp2":cold2,
-                "vol":vol,
-                "current":current
-            }
-            client.publish("powerstation",JSON.stringify(data))
 
-        }, 10000);
+        setInterval(() => {
+
+            for (let i = 1; i <= 3; i++) {
+
+                var hot = Math.floor(Math.random() * (100 - 70 + 1)) + 70;
+                var cold = Math.floor(Math.random() * (20 - 0 + 1)) + 0;
+                var cold2 = Math.floor(Math.random() * (20 - 0 + 1)) + 0;
+                var vol = 5
+                var current = 1
+                var data = {
+                    "station_id": i,
+                    "hot_water_temp": hot,
+                    "cold_water_temp": cold,
+                    "cold_water_temp2": cold2,
+                    "vol": vol,
+                    "current": current
+                }
+                client.publish("powerstation", JSON.stringify(data))
+
+            }
+
+
+        }, 60000);
     })
 })
 
